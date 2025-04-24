4bb06cf75fc36907c5cc6a54ee7ef00805ecd1ea
Tirza Samosir <samosirtirza019@gmail.com>
2025-04-24 11:32:30 +0200

    add state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
           <Dictionary />
         </main>
       </div>
+      <footer className="App-footer">Coded with 🤍 by
+        <a href="https://tirzasamosir.netlify.app/" target="_blank"> Tirza S.</a>
+      </footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css"
 
 export default function Dictionary() {
+    let [keyword, setKeyword] = useState("")
+
+    function handleKeyWordChange(event) {
+        setKeyword(event.target.value);
+    }
+
+    function search(event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword}`)
+    }
+
     return (
-        <div>Hello from Dictionary!</div>
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeyWordChange} />
+            </form>
+        </div>
     )
 
 }
